70655646f688863ff0ec0f4cefa2819b5dfd7acd
Aron Bakes <aronbakes@gmail.com>
2026-04-08 10:56:27 +1000

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,54 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        # output = [1]*len(nums)
+
+        # for i in range(len(nums)):
+        #     out = 1
+        #     for j, n in enumerate(nums):
+        #         if i==j:
+        #             continue
+        #         else:
+        #             out = n*out
+
+        #     output.append(out)
+        #     out = 1
+
+        # num only has 41 possible values
+
+                
+        # nums  = [-1,  0,  1,  2,  3]
+        # p     = [ 1, -1,  0,  0,  0]
+        # s     = [ 0,  6,  6,  3,  1]
+        # r     = [ 0, -6,  0,  0,  0]
+
+        p_arr = [1] * len(nums)
+        s_arr = [1] * len(nums)
+        r_arr = [1] * len(nums)
+
+        p = 1
+        s = 1
+
+        for i in range(len(nums)):
+            p_arr[i] = p
+            p = p * nums[i]
+
+        for j in range(len(nums) -1, -1, -1):
+            s_arr[j] = s
+            s = s * nums[j]
+
+            r_arr[j] = p_arr[j] * s_arr[j]
+
+
+
+        return r_arr
+
+        
+
+            
+            
+
+
+
+
+
